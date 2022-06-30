301fca262b6a7e99c5908982d95d1ac2ffd6a1ac
supersett <nolang3748@naver.com>
2022-06-30 14:43:41 +0900

    first commit

---
+++ kemeet/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('achievement/',include("achievement.urls")),
+    path('apply/',include("apply.urls")),
+    path('review/',include("review.urls")),
+    path('room/',include("room.urls")),
+    path('user/',include("user.urls")),
+]
